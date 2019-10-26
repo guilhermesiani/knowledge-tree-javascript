aa678c3796dbf9dc5e29453a4489538882437560
Guilherme Siani <guilherme@siani.com.br>
2019-10-25 21:55:14 -0300

    Separate tests of builder

--- tests/unit/DesignPatterns/Creational/Builder/RpgCharacter/Builder.test.js
+++ tests/unit/DesignPatterns/Creational/Builder/RpgCharacter/Builder.test.js
@@ -1,18 +1,33 @@
 const Builder = require('../../../../../../src/DesignPatterns/Creational/Builder/RpgCharacter/Builder');
 const Warrior = require('../../../../../../src/DesignPatterns/Creational/Builder/RpgCharacter/Warrior');
 
-test('Test build warrior with weapon and armor', () => {
+test('Test build warrior get instance', () => {
+    const warriorName = 'Nabuco Solimon';
+
+    const builder = new Builder;
+    builder.buildWarrior(warriorName);
+    const warrior = builder.getWarrior();
+    expect(warrior).toBeInstanceOf(Warrior);
+    expect(warrior.name).toEqual(warriorName);
+});
+
+test('Test build warrior with weapon', () => {
+    const warriorName = 'Nabuco Solimon';
+
+    const builder = new Builder;
+    builder.buildWarrior(warriorName);
+    builder.buildWeapon(14);
+    const warrior = builder.getWarrior();
+    expect(warrior.attack()).toEqual('Caused 64 of damage');
+});
+
+test('Test build warrior with armor', () => {
     const warriorName = 'Nabuco Solimon';
 
     const builder = new Builder;
     builder.buildWarrior(warriorName);
     builder.buildArmor(43);
-    builder.buildWeapon(14);
     const warrior = builder.getWarrior();
-    expect(warrior).toBeInstanceOf(Warrior);
-    expect(warrior.name).toEqual(warriorName);
-    expect(warrior.life).toEqual(100);
     warrior.receiveDamage(110);
     expect(warrior.life).toEqual(85);
-    expect(warrior.attack()).toEqual('Caused 64 of damage');
 });
